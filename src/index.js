cda72cd4082a196c98a38e2ccec3eb5b879aa9af
Cameron Gleason <camerongleason3@gmail.com>
2024-08-20 10:53:00 -0700

    Initial commit with basic project structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const clientId = '132666'; // Replace with your Strava client ID
-const redirectUri = 'YOUR_REDIRECT_URI'; // Will be your Vercel URL later
+const redirectUri = 'strava-team-challenges.vercel.app/api/exchangeToken'; // Will be your Vercel URL later
 
 document.getElementById('strava-signin').addEventListener('click', () => {
   window.location.href = `https://www.strava.com/oauth/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=activity:read_all`;
