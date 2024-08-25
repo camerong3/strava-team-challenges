f9fa89240fe662a25d6f783e421eb56c92d3ec0e
Cameron Gleason <camerongleason3@gmail.com>
2024-08-25 10:01:20 -0700

    fix UI

--- public/dashboard.js
+++ public/dashboard.js
@@ -293,7 +293,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const header = document.querySelector('.header');
 
   window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) {  // Adjust this value as needed
+    if (window.scrollY > 15) {  // Adjust this value as needed
       header.classList.add('scrolled');
     } else {
       header.classList.remove('scrolled');
